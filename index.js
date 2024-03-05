b6d8f4a14d6cf00e13db0573bb50908206979cc6
bobbyhadz <opmftw@gmail.com>
2024-03-05 07:15:19 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const yesBtn = document.getElementById('yes');
+
+// ✅ Set the radio button to checked
+yesBtn.checked = true;
+
+// ✅ Set the radio button to unchecked
+// yesBtn.checked = false;
